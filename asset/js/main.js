84feed17dcb1a9ad0b2c0456a87fbd08b3836476
Fahmifisall <fakrulfahmi.fisal@gmail.com>
2023-01-19 07:28:36 +0800

    Corporate Business Website
    1. FAKRUL FAHMI BIN FISAL - 2020640844
    2. MUHAMMAD SYAHIR BIN SHARWAD -  2020452454
    3. MUHAMMAD HAZIQ ZAKWAN BIN MAT ZAKI - 2020860944
    4. MUHAMMAD FAKHRI BIN MOHAMMAD FAIZUL - 2020830034

--- asset/js/main.js
+++ asset/js/main.js
@@ -124,3 +124,11 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+$('.dropdown-toggle').click(function(e) {
+	e.preventDefault();
+	setTimeout($.proxy(function() {
+	  if ('ontouchstart' in document.documentElement) {
+		$(this).siblings('.dropdown-backdrop').off().remove();
+	  }
+	}, this), 0);
+  });
